fc7da97e51f302fefd5385e7780bd9f953e12512
amang06 <aman@skynetiks.com>
2024-09-14 13:12:54 +0530

    test connection

--- db.ts
+++ db.ts
@@ -3,16 +3,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
+});
+pool.query("SELECT 1", (err, res) => {
+  if (err) {
+    console.error("Unable to connect to the database:", err);
+  } else {
+    console.log("Database connection successful");
+  }
 });
 
 export const query = async (text: string, params: (string | number)[]) => {
-	try {
-		const result = await pool.query(text, params);
-		return result;
-	} catch (error) {
-		throw error;
-	}
+  try {
+    const result = await pool.query(text, params);
+    return result;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default pool;
